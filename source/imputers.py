10b647ac490940cf9c7737d22d5aae88e8199a13
braunjon <25931090+braunjon@users.noreply.github.com>
2020-10-08 22:34:15 +0200

    Added KNNImputer

--- source/imputers.py
+++ source/imputers.py
@@ -5,18 +5,17 @@ Data imputer to set values for NaN values in datasets
 __author__ = 'Andreas Kaufmann, Jona Braun, Sarah Morillo'
 __email__ = "ankaufmann@student.ethz.ch, jonbraun@student.ethz.ch, sleonardo@student.ethz.ch"
 
-
 from logcreator.logcreator import Logcreator
 import numpy as np
-from sklearn.impute import SimpleImputer
+from sklearn.impute import SimpleImputer, KNNImputer
 import pandas as pd
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 
+
 class Imputer:
     def __init__(self):
         Logcreator.info("Imputer initialized")
-
 
     def mean_simple_imputer(self, x_train, y_train, x_test):
         imp = SimpleImputer(missing_values=np.nan, strategy='mean')
@@ -41,7 +40,8 @@ class Imputer:
         return x_train_imputed, y_train, x_test_imputed
 
     def multivariate_imputer(self, x_train, y_train, x_test):
-        Logcreator.info("Nr. of rows without any NaN: {} \n".format(x_train.shape[0] - sum([True for idx, row in x_train.iterrows() if any(row.isna())])))
+        Logcreator.info("Nr. of rows without any NaN: {} \n".format(
+            x_train.shape[0] - sum([True for idx, row in x_train.iterrows() if any(row.isna())])))
 
         imp = IterativeImputer(missing_values=np.nan, max_iter=1, sample_posterior=False, random_state=0)
         imp.fit(x_train)
@@ -50,4 +50,12 @@ class Imputer:
         IterativeImputer(random_state=0)
         x_train_imputed = imp.transform(x_train)
         x_test_imputed = imp.transform(x_test)
+        return x_train_imputed, y_train, x_test_imputed
+
+    def knn_imputer(self, x_train, y_train, x_test):
+        imputer = KNNImputer(n_neighbors=40, weights="uniform")
+
+        x_train_imputed = imputer.fit_transform(x_train)
+        x_test_imputed = imputer.transform(x_test)
+
         return x_train_imputed, y_train, x_test_imputed

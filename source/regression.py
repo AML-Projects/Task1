2594d37cb220fdaa691d2536518bf3eb107434b6
braunjon <25931090+braunjon@users.noreply.github.com>
2020-10-08 22:51:51 +0200

    Added StratifiedSplit support not tested

--- source/regression.py
+++ source/regression.py
@@ -5,27 +5,73 @@ Regression is performd in this class
 __author__ = 'Andreas Kaufmann, Jona Braun, Sarah Morillo'
 __email__ = "ankaufmann@student.ethz.ch, jonbraun@student.ethz.ch, sleonardo@student.ethz.ch"
 
-from logcreator.logcreator import Logcreator
-from sklearn.linear_model import Ridge
-from sklearn import model_selection
 import math
 
+import pandas as pd
+from sklearn import model_selection
+from sklearn.linear_model import Ridge
+
+from logcreator.logcreator import Logcreator
 
 
 class Regression:
     def __init__(self):
         Logcreator.info("Start fit model")
 
-
     def ridge_regression(self, x_train, y_train, x_test, handin):
         if not handin:
-            x_train_split, x_test_split, y_train_split, y_test_split = model_selection.train_test_split(
-            x_train, y_train, test_size=0.1, random_state=43)
+            # TODO check if stratified split works, than set to True
+            stratified_split = False
+            if stratified_split:
+                # convert to dataframe if they are not already
+                x_train = pd.DataFrame(x_train)
+                y_train = pd.DataFrame(y_train)
+                x_test = pd.DataFrame(x_test)
+
+                # Remove samples for which we have only one age class,
+                # so that we can do a stratified split
+                y_train_int = y_train.astype(int)
+                counts = y_train_int["y"].value_counts()
+                Logcreator.info("The number of samples for each 'age/class' are \n ", pd.DataFrame(counts).T)
+                classes_with_one_el = counts[counts == 1].index.values
+                Logcreator.info("'Age' with only one sample \n ", classes_with_one_el)
+                remove_columns = y_train_int["y"].isin(classes_with_one_el)
+
+                # Save samples/rows we remove
+                train_data_x_removed = x_train[remove_columns]
+                train_data_y_removed = y_train[remove_columns]
+
+                # Remove samples/rows
+                y_train_int = y_train_int[~remove_columns]
+                y_train = y_train[~remove_columns]
+                x_train = x_train[~remove_columns]
+
+                # Do the stratified split without those samples
+                x_train_split, x_test_split, y_train_split, y_test_split = model_selection.train_test_split(
+                    x_train, y_train, stratify=y_train_int, test_size=0.1, random_state=43)
+
+                # Append the removed samples/rows to the training split
+                x_train_split = x_train_split.append(train_data_x_removed)
+                y_train_split = y_train_split.append(train_data_y_removed)
+
+                # TODO maybe move this code to where we need it
+                # reset all indexes(=start from 0) because some transformers remove the index
+                # and because y is not part of every transformation, the index is not removed for y
+                # which can causes problems
+                x_train_split.reset_index(drop=True, inplace=True)
+                x_test_split.reset_index(drop=True, inplace=True)
+                y_train_split.reset_index(drop=True, inplace=True)
+                y_test_split.reset_index(drop=True, inplace=True)
+
+            else:
+                x_train_split, x_test_split, y_train_split, y_test_split = model_selection.train_test_split(
+                    x_train, y_train, test_size=0.1, random_state=43)
         else:
             x_train_split = x_train
             x_test_split = x_test
             y_train_split = y_train
             y_test_split = None
+
         Logcreator.info("x_train_split: {}".format(x_train_split.shape))
         Logcreator.info("x_test_split: {}".format(x_test_split.shape))
 

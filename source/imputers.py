3433027f716c005f0e190acf2ae1a4eed745d3cc
braunjon <25931090+braunjon@users.noreply.github.com>
2020-10-14 00:25:03 +0200

    Enabled iterative imputer again

--- source/imputers.py
+++ source/imputers.py
@@ -8,6 +8,7 @@ __email__ = "ankaufmann@student.ethz.ch, jonbraun@student.ethz.ch, sleonardo@stu
 import numpy as np
 import pandas as pd
 from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from sklearn.impute import SimpleImputer, KNNImputer
 
